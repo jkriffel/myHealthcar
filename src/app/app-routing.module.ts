6f6468208b3d4a0f790bb199f79b9cc2f32b0c1c
WrenPriest <wrenpriest@gmail.com>
2023-02-14 14:44:19 -0600

    basics of splash page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SplashComponent } from "./splash/splash.component";
+import { ProvidersComponent } from "./providers/providers.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/splash', pathMatch: 'full' },
-  { path: 'splash', component: SplashComponent }
+  { path: 'splash', component: SplashComponent },
+  { path: 'providers', component: ProvidersComponent }
 ];
 
 @NgModule({
--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-splash',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./splash.component.css']
 })
 export class SplashComponent {
+  constructor(private readonly router:Router) {
+  }
 
+  goToProviders(languageCode: string): void{
+    console.log(languageCode) //TODO: make links to localized version
+    void this.router.navigate(['/providers'])
+  }
 }
