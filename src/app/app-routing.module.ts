f96fe8a67a97b027f7146ee7384570ed5efcd2ba
WrenPriest <wrenpriest@gmail.com>
2023-02-14 14:02:26 -0600

    added splash routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SplashComponent } from "./splash/splash.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/splash', pathMatch: 'full' },
+  { path: 'splash', component: SplashComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
